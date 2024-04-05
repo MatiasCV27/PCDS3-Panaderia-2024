d8907c176c3bbed99a2a2da2c1de104831d4af5e
Matias Criollo <matiascriollo57@gmail.com>
2024-04-04 22:31:48 -0500

    Hacer el registro cuando se paga y mostrando en historial

--- PCDS2-Panaderia/Data/FacturaData.cs
+++ PCDS2-Panaderia/Data/FacturaData.cs
@@ -6,6 +6,34 @@ namespace PCDS2_Panaderia.Data
 {
     public class FacturaData
     {
+        public List<FacturaModel> ListarFactura()
+        {
+            var oLista = new List<FacturaModel>();
+            var cn = new Conexion();
+            using (var con = new SqlConnection(cn.getCadenaSQL()))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("sp_ListarFactura", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oLista.Add(new FacturaModel()
+                        {
+                            idFactura = Convert.ToInt32(dr["idFactura"]),
+                            usuario = dr["usuario"].ToString(),
+                            costo = Convert.ToInt32(dr["costo"]),
+                            descripcion = dr["descripcion"].ToString(),
+                            fecha = dr["fecha"].ToString(),
+                        });
+                    }
+                }
+            }
+            return oLista;
+        }
+
         public bool GuardarFactura(FacturaModel oFactura)
         {
             bool rpta;
