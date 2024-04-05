3fdadd0c4f390f1a3614fc57b23390fa1e3a1c7c
Matias Criollo <matiascriollo57@gmail.com>
2024-04-04 19:15:32 -0500

    Insertando información a la db al hacer la compra

---
+++ PCDS2-Panaderia/Models/FacturaModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Build.Framework;
+
+namespace PCDS2_Panaderia.Models
+{
+    public class FacturaModel
+    {
+
+        [Required]
+        public int idFactura { get; set; }
+        [Required]
+        public string? usuario { get; set; }
+        [Required]
+        public int costo { get; set; }
+        [Required]
+        public string? descripcion { get; set; }
+        [Required]
+        public string? fecha { get; set; }
+
+    }
+}
